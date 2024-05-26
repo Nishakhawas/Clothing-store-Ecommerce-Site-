e06507d2e32b6a1e2f3ccbad314d7cfce6c11cda
nisha-khawas <kkhawasnisha118@gmail.com>
2024-05-26 21:57:22 +0545

    women product added

--- src/pages/ShopWomen.jsx
+++ src/pages/ShopWomen.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import './CSS/shopwomen.css'
+import women3 from '../components/Assets/women3.jpg'
 export default function ShopWomen() {
   return (
       <div className='shop-category'>
@@ -9,6 +10,9 @@ export default function ShopWomen() {
       <div>
       <button >Explore now</button>
       </div>
+      <div className='women'>
+        <img  src={women3} alt="" />
+        </div>
     </div>
   )
 }

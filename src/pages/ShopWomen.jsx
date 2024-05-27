a80f3319652f0f00fb0442fe395507c75566e2c2
nisha-khawas <kkhawasnisha118@gmail.com>
2024-05-27 21:03:37 +0545

    [BUG] fix

--- src/pages/ShopWomen.jsx
+++ src/pages/ShopWomen.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './CSS/shopwomen.css'
 import women3 from '../components/Assets/women3.jpg'
+import women1 from '../components/Assets/women1.jpg'
+import women2 from '../components/Assets/women2.jpg'
 export default function ShopWomen() {
   return (
+    <>
       <div className='shop-category'>
       <h3>50% OFF</h3>
       <h4>SHOP NOW</h4>
@@ -14,6 +17,17 @@ export default function ShopWomen() {
         <img  src={women3} alt="" />
         </div>
     </div>
+    <div className='category2'>
+    <div className='explore2'>
+  <img src={women1} alt="" />
+  <img src={women2} alt="" />
+  <img src={women2} alt="" />
+  <img src={women2} alt="" />
+ 
+  </div>
+  </div>
+  </>
   )
+ 
 }
 
